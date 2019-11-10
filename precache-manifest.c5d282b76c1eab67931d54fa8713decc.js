90d9570af840aa5dd2801e6890581faf555fa90a
Kigaru <jakubster9@gmail.com>
2019-11-10 02:38:15 +0000

    Updates

---
+++ precache-manifest.c5d282b76c1eab67931d54fa8713decc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17dfebd8dcac5e2d01f7faec2cf0eb91",
+    "url": "/shopping-wishlist/index.html"
+  },
+  {
+    "revision": "2d72f71ab3cedf7c1666",
+    "url": "/shopping-wishlist/static/css/2.d0f33647.chunk.css"
+  },
+  {
+    "revision": "5db2a4010c3f59db0acb",
+    "url": "/shopping-wishlist/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2d72f71ab3cedf7c1666",
+    "url": "/shopping-wishlist/static/js/2.e671f493.chunk.js"
+  },
+  {
+    "revision": "5db2a4010c3f59db0acb",
+    "url": "/shopping-wishlist/static/js/main.a9f63aff.chunk.js"
+  },
+  {
+    "revision": "1417817b89fc72523c39",
+    "url": "/shopping-wishlist/static/js/runtime-main.3c580a33.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/shopping-wishlist/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/shopping-wishlist/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/shopping-wishlist/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/shopping-wishlist/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/shopping-wishlist/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
